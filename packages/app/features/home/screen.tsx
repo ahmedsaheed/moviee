5d9fbf7fde4510e859203c23d2f6dfbe24762a82
ahmed <ahmedsaheed2@outlook.com>
2024-02-12 08:53:31 +0000

    chore(minor): cleanup

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -7,7 +7,6 @@ import { HeaderComponent, LargeHeaderComponent } from 'app/components/greeting'
 import { ScrollViewWithHeaders } from '@codeherence/react-native-header'
 import { useMovieDataFromCategories } from 'app/hooks/useMovieDataFromCategory'
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
-import { useAsyncStorage } from '@react-native-async-storage/async-storage'
 export function HomeScreen() {
     const {
         trendingToday,
